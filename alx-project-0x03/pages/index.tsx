16cde6e7d56569e2b3ee4ac9df2dde8c845070f7
Hanan <your-email@example.com>
2025-08-09 05:09:04 -0700

    Implement shared layout with Header and Footer

---
+++ alx-project-0x03/pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "../components/layouts/Layout";
+
+const Home: React.FC = () => {
+  return (
+    <Layout>
+      <h1>Welcome to Splash App</h1>
+    </Layout>
+  );
+};
+
+export default Home;
